ace3a1c6ebb204df399a51ebaadca0699c231b80
Novandev <donovan.adams@students.makeschool.com>
2019-02-05 14:58:53 -0800

    finished squarofsums

--- difference-of-squares/diffsquares.go
+++ difference-of-squares/diffsquares.go
@@ -1,5 +1,14 @@
 package diffsquares
 
 func SquareOfSum(num int) int{
-	
+	sumOfSquares := 0
+	squareOfSums := 0
+	diff := 0
+	for i:=1; i < num + 1; i++{
+		squareOfSums += i
+		sumOfSquares += (i*i)
+	}
+	squareOfSums *=squareOfSums
+	diff =(squareOfSums-sumOfSquares)
+	return diff
 }

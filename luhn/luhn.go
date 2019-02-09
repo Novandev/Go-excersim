ea5b64df5ff380090ca788ccc85271e41061767d
Novandev <donovan.adams@students.makeschool.com>
2019-02-09 03:19:28 -0800

    added luhn function and is finally done

---
+++ luhn/luhn.go
@@ -0,0 +1,43 @@
+package luhn
+
+import (
+	"strconv"
+	"strings"
+	// "reflect"
+)
+func Valid(str string) bool{
+	var err error
+	str = strings.Trim(str,"")
+	str = strings.Replace(str, " ", "",-1)
+	str_array := strings.Split(str,"")
+	num_array := make([]int, len(str_array))
+	if len(str) < 2 {
+		return false
+	}
+	for pos,char := range str_array{
+		if pos % 2 == 0{
+			num_array[pos], _ = strconv.Atoi(char)
+			num_array[pos] = num_array[pos]*2
+			if num_array[pos] > 9{
+				num_array[pos] = num_array[pos] -9
+			}
+			if err != nil {
+				return false
+			}
+		}else{
+			num_array[pos], _ = strconv.Atoi(char)
+			if num_array[pos] > 9{
+				num_array[pos] = num_array[pos] -9
+			}
+			if err != nil {
+				return false
+			}
+		}
+
+	}
+	sum := 0
+	for num := range num_array{
+		sum += num
+	}
+	return sum %10 == 0
+}
